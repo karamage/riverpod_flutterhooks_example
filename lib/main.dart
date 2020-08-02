2e1c931325f92fc6079932e0c880e9cefdbf19f3
karamage <karamage.umauma@gmail.com>
2020-08-02 09:44:59 +0900

    cut out counter.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:state_notifier/state_notifier.dart';
-
-final counterProvider = StateNotifierProvider((_) => Counter());
-
-class Counter extends StateNotifier<int> {
-  Counter() : super(0);
-  void increment() => state++;
-}
+import 'providers/counter.dart';
 
 void main() {
   runApp(

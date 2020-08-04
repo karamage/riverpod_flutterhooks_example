e36d866905553038dd6ad7236dc9e0d27b07246b
karamage <karamage.umauma@gmail.com>
2020-08-04 09:05:30 +0900

    tweak test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:riverpod_flutterhooks_example/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(CounterApp());
+    await tester.pumpWidget(ProviderScope(child: CounterApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
